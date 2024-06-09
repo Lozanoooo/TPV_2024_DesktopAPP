87c5f7e2a385116a41576935b990e5b2fab516df
D1CH40 W4N9 <68510103+DWMMI@users.noreply.github.com>
2024-06-09 03:46:15 +0200

    Estética en ventas.fxml

--- src/main/java/com/example/tpv_2024/Servicio/VentasService.java
+++ src/main/java/com/example/tpv_2024/Servicio/VentasService.java
@@ -1,4 +1,15 @@
 package com.example.tpv_2024.Servicio;
 
+/*
+* Esta clase se encarga de registrar las ventas de los productos (Con PK: idEmpleado → empleado; idCliente → cliente; codigoBarra → productos)
+* en la base de datos del servidor
+*
+* */
+
 public class VentasService {
+    private static final String BASE_URL = "http://localhost:8080/";
+    public static void registrarVenta(String idEmpleado, String idCliente, String codigoBarra) {
+        // Aquí se debe implementar la lógica para registrar la venta en la base de datos
+        System.out.println("Registrando venta: idEmpleado=" + idEmpleado + ", idCliente=" + idCliente + ", codigoBarra=" + codigoBarra);
+    }
 }
